902dab0309c59358537e7882d1659b96279c20d3
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-04-04 09:47:17 -0700

    reach out

--- testMirror/routes/index.js
+++ testMirror/routes/index.js
@@ -1,10 +1,13 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+let file = require('./../../constants')
+
+let someValue = file ? file : 'meh'
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'testMirror' });
+    res.render('index', { title: 'testMirror', signal: someValue});
 });
 
 module.exports = router;
