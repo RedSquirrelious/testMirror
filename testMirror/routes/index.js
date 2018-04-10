3d0355088e17342240bbb5a72216f6f8dd5a566d
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-04-10 07:45:01 -0700

    test console

--- testMirror/routes/index.js
+++ testMirror/routes/index.js
@@ -5,6 +5,11 @@ let file = require('./../../../constants')
 
 let someValue = file ? file : 'meh'
 
+console.log('*****************************')
+console.log('helloooooooo')
+console.log('?????')
+console.log('*****************************')
+
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'testMirror', signal: someValue});
